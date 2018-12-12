44592ac408159138a693aee8c8b2b8ffeb5d2589
santosh <santosh.ece06@gmail.com>
2018-12-12 16:17:51 +0530

    basic movment.

--- game/1.js
+++ game/1.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+
+
+setInterval(()=>{
+
+    let current = document.querySelector(".animated");
+    
+    if(current) {
+        current.classList.remove("border-dark", "animated", "infinite", "pulse", "slower");
+
+        let next = current.nextElementSibling;
+        if(next) {
+            next.classList.add("border-dark", "animated", "infinite", "pulse", "slower");
+        }
+    }
+
+}, 2000);
