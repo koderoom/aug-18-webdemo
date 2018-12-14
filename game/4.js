4ab270a7b8c2258d86e4a5d82484326d29c60d01
santosh <santosh.ece06@gmail.com>
2018-12-14 17:31:41 +0530

    4 done.

--- game/4.js
+++ game/4.js
@@ -1,3 +1,10 @@
+let refChild = `<img src="2.png" style="object-fit: cover; height:100%;">`;
+
+window.onload = ()=>{
+    let current = document.querySelector('.active-pointer');
+    current.innerHTML = refChild;
+}
+
 setInterval(()=>{
     let rnumber = Math.floor((Math.random() * 4) + 1);
     console.log("Random #", rnumber);
@@ -10,10 +17,12 @@ let move = (rnumber)=>{
     setTimeout(() => {
         if(rnumber){
             let current = document.querySelector('.active-pointer');
-            current.classList.remove("active-pointer", "bg-dark", "text-light");
+            current.classList.remove("active-pointer", "bg-dark");
+            current.innerHTML = "";
 
             let next = current.nextElementSibling || document.querySelector(".col-3");
-            next.classList.add("active-pointer", "bg-dark", "make-center", "text-light");
+            next.classList.add("active-pointer", "bg-dark", "make-center");
+            next.innerHTML = refChild;
 
             rnumber = rnumber -1;
             move(rnumber);
