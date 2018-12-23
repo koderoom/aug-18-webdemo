d6076f291904c4d02dbedee067c569af24d38d4c
santosh <santosh.ece06@gmail.com>
2018-12-23 14:04:24 +0530

    element movement.

--- hrishi/main.js
+++ hrishi/main.js
@@ -2,5 +2,16 @@ window.addEventListener('keyup', function(event){
 
     if(event.keyCode === 13) {
         console.log("HEllo Hrishi!!!");
+
+        const innerElement = `<img src="1.jpeg" class="img-fluid"  style="object-fit: cover; width:60%">`;
+
+        const selElement = document.querySelector(".sel");
+        selElement.innerHTML = "";
+        selElement.classList.remove("sel");
+
+        const nextElement = selElement.nextElementSibling;
+        console.log(nextElement);
+        nextElement.innerHTML = innerElement;
+        nextElement.classList.add("make-center", "sel");
     }
 });
