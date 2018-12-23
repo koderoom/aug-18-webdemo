2c0a95ae6b1c4829471abf9563ba37362c157fc2
santosh <santosh.ece06@gmail.com>
2018-12-23 14:07:41 +0530

    timout

--- hrishi/main.js
+++ hrishi/main.js
@@ -5,13 +5,16 @@ window.addEventListener('keyup', function(event){
 
         const innerElement = `<img src="1.jpeg" class="img-fluid"  style="object-fit: cover; width:60%">`;
 
-        const selElement = document.querySelector(".sel");
-        selElement.innerHTML = "";
-        selElement.classList.remove("sel");
+        setTimeout(function(){
+            const selElement = document.querySelector(".sel");
+            selElement.innerHTML = "";
+            selElement.classList.remove("sel");
 
-        const nextElement = selElement.nextElementSibling;
-        console.log(nextElement);
-        nextElement.innerHTML = innerElement;
-        nextElement.classList.add("make-center", "sel");
+            const nextElement = selElement.nextElementSibling;
+            console.log(nextElement);
+            nextElement.innerHTML = innerElement;
+            nextElement.classList.add("make-center", "sel");
+        }, 200);
+        
     }
 });
