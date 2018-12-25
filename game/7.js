1650f1cfab2e7cf86b58844b4c95129d6bf8e1c8
santosh <santosh.ece06@gmail.com>
2018-12-25 17:21:51 +0530

    animate 2

--- game/7.js
+++ game/7.js
@@ -1,6 +1,7 @@
 window.onload = ()=>{
     try{
-        uiAnimate();
+        uiAnimate1();
+        uiAnimate2();
     }catch(err){
         console.log(err);
     }
@@ -10,19 +11,58 @@ window.addEventListener('keyup', (event)=>{
     if(event.keyCode == '13'){
         console.log("ENTER PRESSED");
         
-        uiAnimate();
+        uiAnimate1();
+        uiAnimate2();
     }
 });
 
 
-let uiAnimate = ()=>{
+let uiAnimate1 = ()=>{
     try{
         console.log("UI ANIMATE!!!");
 
         const elem1 = document.querySelector("#id1");
         console.log(elem1);
 
-        elem1.style.animation = "balltranslate 2s 20";
+        anime({
+            targets: elem1,
+            translateX: [
+              { value: 0, duration: 0 },
+              { value: 500, duration: 2200 },
+              { value: 0, duration: 1800 }
+            ],
+            rotate: '5turn',
+            backgroundColor: '#FFF',
+            duration: 4000,
+            loop: true
+          
+        });
+    }catch(err){
+        console.log(err);
+    }
+}
+
+
+let uiAnimate2 = ()=>{
+    try{
+        console.log("UI ANIMATE!!!");
+
+        const elem1 = document.querySelector("#id2");
+        console.log(elem1);
+
+        anime({
+            targets: elem1,
+            translateX: [
+              { value: 0, duration: 0 },
+              { value: 500, duration: 2200 },
+              { value: 0, duration: 1800 }
+            ],
+            rotate: '5turn',
+            backgroundColor: '#FFF',
+            duration: 4000,
+            loop: true
+          
+        });
     }catch(err){
         console.log(err);
     }
