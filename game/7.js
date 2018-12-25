c4db8d3aba5275f8a31e214cf2a05a7f4b62cc54
santosh <santosh.ece06@gmail.com>
2018-12-25 16:59:25 +0530

    animate.

--- game/7.js
+++ game/7.js
@@ -18,6 +18,11 @@ window.addEventListener('keyup', (event)=>{
 let uiAnimate = ()=>{
     try{
         console.log("UI ANIMATE!!!");
+
+        const elem1 = document.querySelector("#id1");
+        console.log(elem1);
+
+        elem1.style.animation = "balltranslate 2s 20";
     }catch(err){
         console.log(err);
     }
